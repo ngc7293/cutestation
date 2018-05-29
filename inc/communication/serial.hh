60cec877c1be353baa56c9f3533fc99a70afe772
David Bourgault <dav.bour@gmail.com>
2018-05-28 23:10:48 -0400

    Improved behavior when there is no serial port
    * Application no longer hangs on close when there is no /dev/ttyUSB0

--- inc/communication/serial.hh
+++ inc/communication/serial.hh
@@ -6,10 +6,17 @@
 #include <cstdint>
 
 class Serial {
+protected:
+    bool error_;
+
 public:
+    Serial();
+
     virtual int read_byte(uint8_t* dest, int retry = 0) = 0;
 
     friend Serial& operator>>(Serial& serial, uint8_t& val);
+
+    bool error() const { return error_; };
 };
 
 #endif // SERIAL_H
