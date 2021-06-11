9536958fceb2d7448574cc42cc668a5327166e56
David Bourgault <dav.bour@gmail.com>
2021-06-10 23:49:52 -0400

    Use Conan to manage third parties (#5)
    * Uses Conan for third party libraries
    
    + Added conanfile
    * Updated CMakeLists to use CONAN_PKG dependencies
    * Also had to do some workarounds to get Qt to work correctly
    * Updated GitHub action
    * Disabled TCP tests using GTest's DISABLED_ feature
    
    + Add step to cache the conan folder in Github Actions, to save much
      time by avoiding having to recompile Qt everytime. (Binary packages
      are not available for our needs)

--- tools/python/test.py
+++ tools/python/test.py
@@ -27,8 +27,7 @@ def main(args):
 
     packet = Packet()
     packet.handshake.name = "python_testing"
-    # packet.handshake.commands.append(Handshake.Command(name="anirniq.remote_control.engine.sequence.start", type=Handshake.Command.Type.BOOL))
-    print(packet.IsInitialized())
+    packet.handshake.commands.append(Handshake.Command(name="anirniq.remote_control.engine.sequence.stop", type=Handshake.Command.Type.BOOL))
     print(json_format.MessageToJson(packet))
 
     sock.send(packet.ByteSize().to_bytes(8, byteorder='little'))
