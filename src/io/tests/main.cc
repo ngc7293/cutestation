824433921fe19231280fb8b51f37113c7bb19bf7
David Bourgault <dav.bour@gmail.com>
2021-06-17 09:48:01 -0400

    Fixes compiler flags to support MSVC
    * Fixes compiler flags to support MSVC and slightly reduce boilerplate

--- src/io/tests/main.cc
+++ src/io/tests/main.cc
@@ -7,4 +7,5 @@ int main(int argc, const char* argv[])
     testing::InitGoogleTest(&argc, (char**)argv);
     int rc = RUN_ALL_TESTS();
     google::protobuf::ShutdownProtobufLibrary();
+    return rc;
 }
